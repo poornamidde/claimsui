6e75ad2c97afa8c41d8a4d88215656ec14ff9260
Poorna Chandra <124190106+poornamidde@users.noreply.github.com>
2023-02-17 20:25:49 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-   CLAIMS_URL:'http://34.205.191.104:9000',
-   FACILITY_URL:'http://34.205.191.104:9001',
-   CUSTOMER_URL:'http://34.205.191.104:9002'
+   CLAIMS_URL:'http:// 44.201.24.72:9000',
+   FACILITY_URL:'http:// 44.201.24.72:9001',
+   CUSTOMER_URL:'http:// 44.201.24.72:9002'
 };
